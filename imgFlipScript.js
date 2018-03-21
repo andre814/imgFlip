a05e7a4cf6fa2b705abed508ba5d0c1cd4b966e8
andre814 <andre814@umn.edu>
2018-03-21 14:57:37 -0500

    in class practice

---
+++ imgFlipScript.js
@@ -0,0 +1,53 @@
+$(document).scroll(function(){
+
+	var y = $(document).scrollTop();
+
+	if (y > 100) {
+		$("#one").fadeIn();
+	}else{
+		$("#one").fadeOut();
+	}
+
+	if (y > 200) {
+		$("#two").fadeIn();
+	}else{
+		$("#two").fadeOut();
+	}
+
+	if (y > 300) {
+		$("#three").fadeIn();
+	}else{
+		$("#three").fadeOut();
+	}
+
+	if (y > 400) {
+		$("#four").fadeIn();
+	}else{
+		$("#four").fadeOut();
+	}
+
+	if (y > 500) {
+		$("#five").fadeIn();
+	}else{
+		$("#five").fadeOut();
+	}
+
+	if (y > 600) {
+		$("#six").fadeIn();
+	}else{
+		$("#six").fadeOut();
+	}
+
+	if (y > 700) {
+		$("#seven").fadeIn();
+	}else{
+		$("#seven").fadeOut();
+	}
+
+	if (y > 800) {
+		$("#eight").fadeIn();
+	}else{
+		$("#eight").fadeOut();
+	}
+
+});
